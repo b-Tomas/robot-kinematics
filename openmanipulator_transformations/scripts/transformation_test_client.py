be191d1c6c73e4fc4624fc28ff5902119ed79f19
Tomás Badenes <41840767+b-Tomas@users.noreply.github.com>
2023-07-04 17:22:06 -0300

    Base package + transformation service (#8)
    * Base package + transformation service
    
    * Remove unused variable
    
    * Applied suggestions

---
+++ openmanipulator_transformations/scripts/transformation_test_client.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import sys
+import rospy
+from openmanipulator_transformations.srv import Transform
+
+
+TRANSFORM_SERVICE_NAME = "/transformations/transform"
+
+# This is a proof of concept script. It is not meant to be used for anything in particular
+def transform_client(vec):
+    rospy.wait_for_service(TRANSFORM_SERVICE_NAME)
+    try:
+        service = rospy.ServiceProxy(TRANSFORM_SERVICE_NAME, Transform)
+        resp = service(vec)
+        return resp.joint_angles
+    except rospy.ServiceException as e:
+        print(f"Service call failed: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        coords = tuple(map(lambda x: float(x), sys.argv[1:]))
+    else:
+        print(f"Usage: {sys.argv[0]} x y z")
+        sys.exit(1)
+    print(f"Requesting: {coords}")
+    print(f"Response: {transform_client(coords)}")
