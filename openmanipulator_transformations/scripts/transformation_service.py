4fc0824228da85285fd4bbb2cfb8f3fbb9a8a191
Tomás Badenes <tomasbadenes@gmail.com>
2023-07-04 16:16:34 -0300

    Base package + transformation service

---
+++ openmanipulator_transformations/scripts/transformation_service.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import rospy
+
+from openmanipulator_transformations.srv import Transform, TransformRequest, TransformResponse
+import numpy as np
+
+TRANSFORM_SERVICE_NAME = "/transformations/transform"
+TRANSFORM_NODE_NAME = "transform_srv"
+
+
+# Computation service. Converts cartesian coordinates to joint angles using the matrix below
+
+matrix = np.array([
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+    [0, 0, 0],
+])
+
+
+def transform(vec: tuple) -> tuple:
+    # Operation:
+    #   matrix (4 by 3) . vec (3 by 1)
+    # Vec should be a one dimensional array of three elements
+    return np.matmul(matrix, np.asarray(vec))
+
+
+def process_service_request(req: TransformRequest) -> TransformResponse:
+    rospy.loginfo(f"Received request: {req}")
+    response = TransformResponse()
+    response.joint_angles = transform(req.coordinates)
+    rospy.loginfo(f"Generated response: {response}")
+    return response
+
+
+def serve():
+    # ROS node for the service server.
+    rospy.init_node(TRANSFORM_NODE_NAME, anonymous=False)
+
+    # Create a ROS service type.
+    rospy.Service(TRANSFORM_SERVICE_NAME, Transform, process_service_request)
+
+    # Log message about service availability.
+    rospy.loginfo(f"{TRANSFORM_SERVICE_NAME} service is now available.")
+    rospy.spin()
+
+
+if __name__ == "__main__":
+    serve()
---
+++ openmanipulator_transformations/scripts/transformation_test_client.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import sys
+import rospy
+from openmanipulator_transformations.srv import Transform, TransformRequest, TransformResponse
+
+
+TRANSFORM_SERVICE_NAME = "/transformations/transform"
+TRANSFORM_NODE_NAME = "transform_srv"
+
+# This is a proof of concept script. It is not meant to be used for anything in particular
+def transform_client(vec):
+    rospy.wait_for_service(TRANSFORM_SERVICE_NAME)
+    try:
+        service = rospy.ServiceProxy(TRANSFORM_SERVICE_NAME, Transform)
+        resp = service(vec)
+        return resp.joint_angles
+    except rospy.ServiceException as e:
+        print(f"Service call failed: {e}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 4:
+        x = int(sys.argv[1])
+        y = int(sys.argv[2])
+        z = int(sys.argv[3])
+    else:
+        print(f"Usage: {sys.argv[0]} x y z")
+        sys.exit(1)
+    coords = (x, y, z)
+    print(f"Requesting: {coords}")
+    print(f"Response: {transform_client(coords)}")
