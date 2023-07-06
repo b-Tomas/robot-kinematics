4add8b1ac7a221780c912da9613a6364731a7895
Tomás Badenes <tomasbadenes@gmail.com>
2023-07-06 09:49:00 -0300

    Math

---
+++ openmanipulator_transformations/scripts/test.py
@@ -0,0 +1,54 @@
+# TODO: Move tests somewhere else. And if possible use an actual testing framework
+# Run this script as plain python script from within the container
+
+from transformation_service import *
+from math import atan, atan2, cos, acos, sin, sqrt, floor, pi
+import numpy as np
+
+
+def test_transform():
+    def test(transformed, expected):
+        print(transformed, expected)
+        for i in range(len(expected)):
+            print(transformed[i], expected[i])
+            assert floor(transformed[i]*100) == floor(expected[i]*100)
+
+    transformed = transform((1, 0, 1, -pi/2), .5, 1, 1, .5, 0, 0, 0)
+    expected = (0, 0, 0, pi/2)
+    assert len(transformed), len(expected)
+    test(transformed, expected)
+    
+    transformed = transform((1.5, 0, 1.5, 0), .5, 1, 1, .5, 0, 0, 0)
+    expected = (0, 0, 0, 0)
+    assert len(transformed), len(expected)
+    test(transformed, expected)
+
+    transformed = transform((0.274, 0, 0.2028, 0), 0.0595, 0.13023, 0.124, 0.126, 0, 0, 0)
+    expected = (0, 0, 0, 0)
+    assert len(transformed), len(expected)
+    test(transformed, expected)
+
+    transformed = transform((0.286, 0, 0.2045, 0), 0.0595, 0.13023, 0.124, 0.126, 0.012, 0, 0.017)
+    expected = (0, 0, 0, 0)
+    assert len(transformed), len(expected)
+    test(transformed, expected)
+
+
+test_transform()
+
+def general_mat(d, t, r, a):
+    return np.mat([
+        [cos(t), -sin(t)*cos(a), sin(t)*sin(a), r*cos(t)],
+        [sin(t), cos(t)*cos(a), -cos(t)*sin(a), r*sin(t)],
+        [0, sin(a), cos(a), d],
+        [0, 0, 0, 1]
+    ])
+q1 = 0
+q2 = 0
+q3 = 0
+q4 = 0
+l1 = 0.0595
+l2 = 0.13023
+l3 = .124
+l4 = .126
+print(general_mat(l1, q1, 0, pi/2)*general_mat(0, q2+pi/2, l2, 0)*general_mat(0, q3-pi/2, l3, 0)*general_mat(0, q4, l4, 0))
--- openmanipulator_transformations/scripts/transformation_service.py
+++ openmanipulator_transformations/scripts/transformation_service.py
@@ -4,6 +4,7 @@ import rospy
 
 from openmanipulator_transformations.srv import Transform, TransformRequest, TransformResponse
 import numpy as np
+from math import atan, atan2, cos, acos, sin, sqrt, pi
 
 # Computation service. Converts cartesian coordinates to joint angles using the data below
 
@@ -20,30 +21,61 @@ JOINT_4_ORIGIN = np.array([0.124, 0, 0])      # Child of link3, parent of link5 
 JOINT_END_EFFECTOR = np.array([0.126, 0, 0])  # Child of link5, which starts at joint4
 
 # Link2 is the one above joint1 and because of that it rotates around its longitudinal axis. It is always pointing up
-LINK_2_LONGITUDE =  np.linalg.norm(JOINT_2_ORIGIN)
-LINK_3_LONGITUDE =  np.linalg.norm(JOINT_3_ORIGIN)
-LINK_4_LONGITUDE =  np.linalg.norm(JOINT_4_ORIGIN)
+LINK_2_LONGITUDE = np.linalg.norm(JOINT_2_ORIGIN)
+LINK_3_LONGITUDE = np.linalg.norm(JOINT_3_ORIGIN)
+LINK_4_LONGITUDE = np.linalg.norm(JOINT_4_ORIGIN)
 # Distance from joint 4 to the center of the end-effector (the red cube in gazebo)
 LAST_BIT_LONGITUDE =  np.linalg.norm(JOINT_END_EFFECTOR)
 
-matrix = np.array([
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-    [0, 0, 0],
-])
+# These are the final paramets used for computation
+L1 = LINK_2_LONGITUDE
+L2 = LINK_3_LONGITUDE
+L3 = LINK_4_LONGITUDE
+L4 = LAST_BIT_LONGITUDE
+OFFSET_X, OFFSET_Y, OFFSET_Z = JOINT_1_ORIGIN
 
-def transform(vec: tuple) -> tuple:
-    # Operation:
-    #   matrix (4 by 3) . vec (3 by 1)
-    # Vec should be a one dimensional array of three elements
-    return np.matmul(matrix, np.asarray(vec))
+
+def transform(vec: tuple, l1, l2, l3, l4, offset_x, offset_y, offset_z) -> tuple:
+    x, y, z, t = vec
+
+    x = x - offset_x
+    z = z - l1 - offset_z # Height of the second joint. This math is relative to the second joint
+    y = y - offset_y
+
+    # Transform (X, Y, Z, t) -> (X', Z') the coordinates in the plane formed by the joints with the origin in the first joint
+    # X' being the horizontal distance from the origin to the end position
+    # We are using cylindrical coordinates now
+    x_t = sqrt(x**2 + y**2)
+    z_t = z
+
+    # Solve the equation system formed by the combination of the forward kinematics transformations
+    w1 = x_t-l4*cos(t)
+    w2 = z_t-l4*sin(t)
+
+    a1 = 2*w1*l2
+    a2 = 2*w2*l2
+    a3 = w1**2+w2**2+l2**2-l3**2
+    
+    print(a1, a2, a3)
+
+    t1 = atan2(a2, a1) + acos(a3/sqrt(a1**2+a2**2))
+    t2 = atan2((w2-l2*sin(t1)), (w1-l2*cos(t1))) - t1
+    t3 = t-t1-t2
+    
+    # Transform back to 3D and apply initial conditions
+    q1 = atan2(y, x)
+    q2 =  pi/2-t1
+    q3 = -pi/2-t2
+    q4 = -t3
+    
+    return (q1, q2, q3, q4)
 
 
 def process_service_request(req: TransformRequest) -> TransformResponse:
     rospy.loginfo(f"Received request: {req}")
+    if len(req.coordinates) != 4: raise ValueError  # TODO: use a more meaningful error
     response = TransformResponse()
-    response.joint_angles = transform(req.coordinates)
+    response.joint_angles = transform(req.coordinates, L1, L2, L3, L4, OFFSET_X, OFFSET_Y, OFFSET_Z)
     rospy.loginfo(f"Generated response: {response}")
     return response
 
