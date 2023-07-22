636ccc22954b6d9b2d35154ee0445cd8f6bc85eb
Tomás Badenes <tomasbadenes@gmail.com>
2023-07-22 16:21:52 -0300

    Tests

--- openmanipulator_transformations/setup.py
+++ openmanipulator_transformations/setup.py
@@ -3,6 +3,8 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-setup_args = generate_distutils_setup(packages=["kinematics"], package_dir={"": "src"})
+setup_args = generate_distutils_setup(
+    packages=["kinematics"], package_dir={"": "src"}, tests_require=["unittest"]
+)
 
 setup(**setup_args)
